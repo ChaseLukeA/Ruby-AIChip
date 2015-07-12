b7ccb5adebb3aea96cfc9adc7027fcb455f3ae66
Brian Hogan / New Auburn Personal Computer Services <bphogan@gmail.com>
2015-07-12 15:13:05 -0500

    Feedback on guess_the_number assignment

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -4,6 +4,8 @@
 # Copyright 2015
 
 # /-------------------------------FUNCTIONS-----------------------------------\
+
+## This is awesome. :)
 def pc_speak(message)
   characters = message.split("")
   characters.each do |char|
@@ -31,6 +33,28 @@ def valid_input(input, regex)
   !!(input =~ regex)
 end
 
+## The multiple returns in this function aren't necessary. And you can improve
+## on this logic using Ruby symbols:
+#
+# def check_answer(guessed_number, actual_number)
+#   if guessed_number == actual_number
+#      :match
+#   elsif guessed_number < actual_number
+#      :low
+#   else
+#      :high
+#   end
+# end
+#
+# Or you could compact it:
+#
+# def check_answer(guessed_number, actual_number)
+#   result = :high # default value
+#   result = :match if guessed_number == actual_number
+#   result = :low if guessed_number < actual_number
+# end
+#
+# Then you can just check the symbol value later.
 def check_answer(guessed_number, actual_number)
   if guessed_number == actual_number
     return 0 # won
@@ -110,6 +134,14 @@ while play_game do
 
     if valid_input(guessed_number, /\A[0-9]+\Z/)
       case check_answer(guessed_number.to_i, actual_number.to_i)
+      ## see, if you use symbols here, you don't need the comments.
+      # when :won
+      #   game_won = true
+      # when :low
+      #   pc_speak.....
+      # when :high
+      #
+      # Symbols are awesome. Use them.
       when 0 # won
         game_won = true
       when 1 # low
@@ -121,13 +153,18 @@ while play_game do
       pc_speak "\n$ I'm sorry, you must enter a valid integer. Please try again."
     end
   end
-
   pc_speak "#{game_won_message(number_of_guesses)}\n\n"
 
   pc_speak "$ Would you like to play another game? "
+  
+  ## Here's where I'd flip the logic to the positive. The =~ in Ruby will evaluate to a true or nil which is false.
+  ## No need for the if statement.
+  # play_game = prompt("\('y' or 'n'\):\n> ") =~ /\A[y|Y]\Z/
   play_game = false if prompt("\('y' or 'n'\):\n> ") =~ /\A[n|N]\Z/
 end
 
+## it seems to me that the $ could be part of the pc_speak function
+## which would then allow you to change the prompt :)
 pc_speak "\n$ Thank you. I enjoyed our interaction.\n"
 sleep 0.50
 pc_speak "\n$ Goodbye.\n\n"
